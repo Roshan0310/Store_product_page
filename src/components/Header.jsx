e9a3de05a4a604249f814789aee180d617d1bd19
Roshan0310 <roshanrosh0310@gmail.com>
2023-06-21 14:09:01 +0530

    mediaQuery added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,36 @@ import React from 'react'
 import "../styles/header.css"
 import {FiUser } from "react-icons/fi"
 import {FaShoppingCart } from "react-icons/fa"
+import { AiOutlineMenu} from "react-icons/ai"
 
 
 const Header = () => {
   return (
     <nav>
-       <h2>STORE</h2>
+      <NavContent/>
 
-       <div>
-        <a href="#home">HOME</a>
-        <a href="#featured">FEATURED</a>
-        <a href="#carousel">ALL PRODUCTS</a>
-        <a href="#contact">CONTACT US</a>
-        <a href="#home"><FaShoppingCart /></a>
-        <a href="#home"><FiUser /></a>
-     
-       </div>
+      <button className='btnn'>
+        <AiOutlineMenu />
+      </button>
     </nav>
   )
 }
 
+
+const NavContent = () => (
+  <>
+
+<h2>STORE</h2>
+
+<div>
+ <a href="#home">HOME</a>
+ <a href="#featured">FEATURED</a>
+ <a href="#carousel">ALL PRODUCTS</a>
+ <a href="#contact">CONTACT US</a>
+ <a href="#home"><FaShoppingCart /></a>
+ <a href="#home"><FiUser /></a>
+
+</div>
+  </>
+)
 export default Header
